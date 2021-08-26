6b9233bc72b742241de082a17eaedf689593e8f9
omar <omar.hfra@gmail.com>
2021-08-26 23:11:26 +0100

    added image url  text field and show the image preview

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -11,11 +11,13 @@ class EditProductScreen extends StatefulWidget {
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
 
   @override
   void dispose() {
     _priceFocusNode.dispose();
     _descriptionFocusNode.dispose();
+    _imageUrlController.dispose();
     super.dispose();
   }
 
@@ -55,6 +57,33 @@ class _EditProductScreenState extends State<EditProductScreen> {
               maxLines: 3,
               keyboardType: TextInputType.multiline,
               focusNode: _descriptionFocusNode,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(right: 5.0,top:8.0),
+                  width: 100.0,
+                  height: 100.0,
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 2.0, color: Colors.grey)),
+                  child: _imageUrlController.text.isEmpty
+                      ? Text('Enter a Url')
+                      : Image.asset(
+                          _imageUrlController.text,
+                          fit: BoxFit.cover,
+                        ),
+                ),
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'ImageUrl',
+                    ),
+                    controller: _imageUrlController,
+                    textInputAction: TextInputAction.done,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
