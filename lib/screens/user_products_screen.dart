a043303151499125b3af896c580eea6b5e3cc089
omar-hadek <omar.hadek7@gmail.com>
2021-09-03 23:20:10 +0100

    added pull to refrech feature

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -7,6 +7,10 @@ import './edit_product_screen.dart';
 class UserProductsScreen extends StatelessWidget {
   const UserProductsScreen({Key? key}) : super(key: key);
   static const routeName = '/user_products';
+  Future<void> _refrechProducts(BuildContext context) async{
+    await Provider.of<Products>(context,listen: false).fetchAndSetProducts();
+  } 
+  
 
   @override
   Widget build(BuildContext context) {
@@ -21,19 +25,22 @@ class UserProductsScreen extends StatelessWidget {
           }),
         ],
       ),
-      body: ListView.builder(
-        itemCount: productsData.length,
-        itemBuilder: (context, i) {
-          return Column(
-            children: [
-              UserProductItem(
-                  id: productsData[i].id,
-                  title: productsData[i].title,
-                  imageUrl: productsData[i].imageUrl),
-              Divider(),
-            ],
-          );
-        },
+      body: RefreshIndicator(
+        onRefresh: () =>_refrechProducts(context),
+        child: ListView.builder(
+          itemCount: productsData.length,
+          itemBuilder: (context, i) {
+            return Column(
+              children: [
+                UserProductItem(
+                    id: productsData[i].id,
+                    title: productsData[i].title,
+                    imageUrl: productsData[i].imageUrl),
+                Divider(),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
