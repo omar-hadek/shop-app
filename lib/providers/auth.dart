efb7d7b74a8402c3f0c5c20d551ca9b7d4c335d4
omar-hadek <omar.hadek7@gmail.com>
2021-09-09 14:27:48 +0100

    implementing sign in function

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -6,15 +8,28 @@ class Auth extends ChangeNotifier{
   late DateTime _expiryDate;
   late String _userId;
 
-  Future<void> signUp(String? email,String?  password) async{
-    final url  = Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAteSP9CcwvPNsampuT0fzHB1vOsjit9-U');
-    http.post(url,body: json.encode(
+  Future<void> _register(String? email,String? password,String queryString )async{
+     
+    final url  = Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:${queryString}?key=AIzaSyAteSP9CcwvPNsampuT0fzHB1vOsjit9-U');
+    final response = await http.post(url,body: json.encode(
       {
         'email':email,
         'password':password,
         'returnSecureToken':true
       }
     ));
+
+    log(json.decode(response.body));
+  }
+
+
+  Future<void> signUp(String? email,String?  password) async{
+    return _register(email, password,'signUp');
+  }
+
+
+  Future<void> signIn(String? email,String? password){
+    return _register(email, password,'signInWithPassword');
   }
 
 }
